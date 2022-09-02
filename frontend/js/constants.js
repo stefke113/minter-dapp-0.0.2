b197a02f487ca1c420627ff194d9d3a62e21c202
stefke113 <stefke113@gmail.com>
2022-09-02 19:11:46 +0200

    1

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -21,7 +21,7 @@ const button_presale_already_minted = "Join The Community";
 const button_public_mint = "Mint Your NFT";
 const mint_failed = "Minting failed. 😢 Please try again.";
 
-const abi = 
+const abi = [
   {
     "abi": [
       {
@@ -1588,4 +1588,4 @@ const abi =
         "signature": "0x476343ee"
       }
     ]
-  }
+  }]
