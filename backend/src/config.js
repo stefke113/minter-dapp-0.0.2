923bdb8b1894645f6e7d79bed1332787782cf89b
stefke113 <stefke113@gmail.com>
2022-09-02 18:52:42 +0200

    contract adress

--- backend/src/config.js
+++ backend/src/config.js
@@ -70,7 +70,7 @@ const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the
 const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
-let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
+let CONTRACT_ADDRESS = "0x2410Afc70269Afe36C8DD53960a9aAe0f13a3Ae6"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = false; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
